6679cfa8be91ff388b08f930d1ce6e6dfd539c62
WesAR <wesley_reboucas@hotmail.com>
2017-09-29 08:38:40 -0300

    Calculator in JavaScript

---
+++ calculadora.js
@@ -0,0 +1,16 @@
+
+var n1=5, n2=3;
+
+
+console.log('------| Calculadora em JavaScript |------');
+
+console.log('Operações fixas:\n');
+res1 = n1 + n2;
+console.log('  [ 1 ] - Soma \n');
+res2 = n1 - n2;
+console.log('  [ 2 ] - Subtração \n');
+res3 = n1 / n2;
+console.log('  [ 3 ] - Divisão \n');
+res2 = n1 * n2;
+console.log('  [ 4 ] - Multipĺicação \n');
+
