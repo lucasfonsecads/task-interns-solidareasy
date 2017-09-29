7d747bb198d3e8aa4847b8adc0236095d373fc8f
WesAR <wesley_reboucas@hotmail.com>
2017-09-29 10:27:06 -0300

    Change in caracters

--- calculadora.js
+++ calculadora.js
@@ -6,11 +6,11 @@ console.log('------| Calculadora em JavaScript |------');
 
 console.log('Operações fixas:\n');
 res1 = n1 + n2;
-console.log(` [ 1 ] - Soma = ${res1} \n`);
+console.log(` [ + ] - Soma = ${res1} \n`);
 res2 = n1 - n2;
-console.log(`  [ 2 ] - Subtração = ${res2} \n`);
+console.log(`  [ - ] - Subtração = ${res2} \n`);
 res3 = n1 / n2;
-console.log(`  [ 3 ] - Divisão = ${res3} \n`);
+console.log(`  [ / ] - Divisão = ${res3} \n`);
 res4 = n1 * n2;
-console.log(`  [ 4 ] - Multipĺicação = ${res4} \n`);
+console.log(`  [ * ] - Multipĺicação = ${res4} \n`);
 
