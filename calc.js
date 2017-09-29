7283117fb1ed5e6941f67bfa6ea3c81cf797c619
˜Volksfeld˜ <jc.mussi99@gmail.com>
2017-09-29 10:25:43 -0300

    js calc

---
+++ calc.js
@@ -0,0 +1,14 @@
+var x = 10, y = 15;
+
+console.log(˜Calculadora:˜);
+
+sum = x + y;
+console.log(`Soma : ${sum}`);
+sub = x - y; 
+console.log(`Subtração : ${sub}`);
+mult = x * y;
+console.log(`Multiplicação : ${mult}`);
+div = x / y;
+console.log(`Divisão : ${div}`);
+mod = x % y;
+console.log(`Resto : ${mod}`);
