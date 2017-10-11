1b133a04b41875eb87270787a0d659a3030c8ff9
WesAR <wesley_reboucas@hotmail.com>
2017-10-11 13:53:13 -0300

    Servidor com express

---
+++ Node-express/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+// Importar modulo express
+fs = require('fs');
+// Para carregar o arquivo html
+app = express();
+// Criar uma aplicação
+
+
+app.get('/', function(req, res){
+/* -> app.get() = método GET do HTTP, e o 
+caminho '/' representando nossa página raiz, 
+seguido da função de callback.
+-> req e res objetos que o node fornece, você pode 
+chamar qualquer coisa que você quiser fazer 
+sem involver o Express.*/
+	res.send('Ola Mundo');
+});
+
+var server = app.listen(3000);
+// Declarando qual porta o servidor vai rodar
+console.log('Servidor Conectado - Porta: %s', server.address().port);
+// Informar, se o servidor conectar, qual a porta
